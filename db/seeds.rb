af80f2a0bed456cdf5fd1fa9e1a314c8c10c7776
charles_mahaco <charles.belpois@gmail.com>
2018-11-26 16:44:19 +0100

    wtf

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,8 @@ require 'faker'
 20.times do
   item = Item.create(title: Faker::Cat.name, description: Faker::Cat.breed, price: Faker::Number.decimal(2), image_url: "http://placekitten.com/300/300")
 end
+
+30.times do
+  user = User.create! :email => Faker::Internet.email, :password => 'topsecret', :password_confirmation => 'topsecret'
+
+end
