32595b9327d691a58fb02f4d74ad3ace4c6e6e2c
TinQk <potie.quentin@gmail.com>
2018-11-27 19:27:49 +0100

    fct remove item, addone, removeone done

--- kittycat-shop/app/models/cart.rb
+++ kittycat-shop/app/models/cart.rb
@@ -1,9 +1,34 @@
 class Cart < ApplicationRecord
+
   belongs_to :user
   has_many :cart_items
   has_many :items, through: :cart_items
 
+  def add_item(item)
+    id = item.id
+    cart_item = cart_items.find_by(item_id: id)
+    if cart_item
+      cart_item.quantity += 1
+      cart_item.save
+    else
+      items << item
+    end
+  end
 
+  def remove_item(cart_item)
+    cart_item.quantity = 0
+    cart_item.save
+  end
+
+  def add_one(cart_item)
+    cart_item.quantity += 1
+    cart_item.save
+  end
+
+  def remove_one(cart_item)
+    cart_item.quantity -= 1
+    cart_item.save
+  end
 
   def total_price
     tot = 0.00
@@ -12,4 +37,5 @@ class Cart < ApplicationRecord
     end
     return tot
   end
+
 end
--- kittycat-shop/app/models/cart_item.rb
+++ kittycat-shop/app/models/cart_item.rb
@@ -1,15 +1,4 @@
 class CartItem < ApplicationRecord
   belongs_to :item
   belongs_to :cart
-
-  def remove_item()
-    self.quantity = 0
-  end
-
-#  def add_to_cart(item)
-#    if
-#    cart.items << item
-#    if find
-#    CartItem.create
-
 end
