0800cd61fd7d5c74c9a0ae6e2b14b9c63cc11603
TinQk <potie.quentin@gmail.com>
2018-11-27 15:22:57 +0100

    total_price working for carts

--- kittycat-shop/app/controllers/carts_controller.rb
+++ kittycat-shop/app/controllers/carts_controller.rb
@@ -4,14 +4,6 @@ class CartsController < ApplicationController
     @cart = current_user.cart
   end
 
-  def total_price()
-    @tot = 0
-    @cart.cart_items.each do |cart_item|
-      @tot += cart_item.item.price
-    end
-    return @tot
-  end
-
   def remove_item(item)
     @cart.items.delete item
   end
--- kittycat-shop/app/models/cart.rb
+++ kittycat-shop/app/models/cart.rb
@@ -2,4 +2,13 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_items
   has_many :items, through: :cart_items
+
+
+  def total_price
+    tot = 0.00
+    cart_items.each do |cart_item|
+      tot += cart_item.item.price.to_f
+    end
+    return tot
+  end
 end
