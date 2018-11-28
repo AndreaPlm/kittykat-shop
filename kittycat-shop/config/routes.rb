f068c58e5d326b30b7ab8b910f3bf470946a8c43
TinQk <potie.quentin@gmail.com>
2018-11-28 17:58:53 +0100

    ca marche pas

--- kittycat-shop/config/routes.rb
+++ kittycat-shop/config/routes.rb
@@ -7,11 +7,12 @@ Rails.application.routes.draw do
 
   # Routes for carts management
   resources :carts, only: [:show]
+  post 'cart/add_item', :action => "add_item_to_cart", :controller => 'carts'
 
-  resources :cart_items, only: [:update, :destroy]
+  resources :cart_items, only: [:create, :update, :destroy]
 
   # get 'profile', action: :show, controller: 'users'
 
   resources :charges, only: [:new, :create]
-  
+
 end
