c67673c198f0f6fdd7b09e2921a9c9d78afac369
TinQk <potie.quentin@gmail.com>
2018-11-29 12:47:44 +0100

    integration add to cart index ok and button to cart ok

--- kittycat-shop/config/routes.rb
+++ kittycat-shop/config/routes.rb
@@ -5,14 +5,11 @@ Rails.application.routes.draw do
   resources :items, only: [:index, :show]
   root 'items#index'
 
-  # Routes for carts management
   resources :carts, only: [:show]
-  post 'cart/add_item', :action => "add_item_to_cart", :controller => 'carts'
 
   resources :cart_items, only: [:create, :update, :destroy]
 
-  # get 'profile', action: :show, controller: 'users'
-
   resources :charges, only: [:new, :create]
+
 
 end
