5184daedc86c9313febf87a43a25a0e3c048ec77
charles_mahaco <charles.belpois@gmail.com>
2018-11-29 15:12:02 +0100

    S3 en production

--- kittycat-shop/db/schema.rb
+++ kittycat-shop/db/schema.rb
@@ -72,6 +72,20 @@ ActiveRecord::Schema.define(version: 2018_11_28_161153) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "items_orders", id: false, force: :cascade do |t|
+    t.bigint "item_id", null: false
+    t.bigint "order_id", null: false
+  end
+
+  create_table "join_table_item_carts", force: :cascade do |t|
+    t.bigint "cart_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_join_table_item_carts_on_cart_id"
+    t.index ["item_id"], name: "index_join_table_item_carts_on_item_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
