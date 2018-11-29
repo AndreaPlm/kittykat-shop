22d089e67a52d5a7c32c28184bb84370f2a8cb0c
MC Le Morvan <mclemorvan@gmail.com>
2018-11-29 17:18:13 +0100

    fixed orders + mailer

--- kittycat-shop/app/controllers/charges_controller.rb
+++ kittycat-shop/app/controllers/charges_controller.rb
@@ -18,21 +18,25 @@ def create
     :currency    => 'usd'
   )
 
-# Créer un order et clear le panier
-  new_order = Order.new
-  new_order.user_id = current_user.id
+    # Créer un order et clear le panier
+      new_order = Order.new
+      new_order.user_id = current_user.id
+      new_order.save
 
-  current_user.cart.cart_items.each do |cart_item|
-    ItemOrder.create(order_id: new_order.id, quantity: cart_item.quantity, item_id: cart_item.item_id)
-    cart_item.destroy
-  end
+      current_user.cart.cart_items.each do |cart_item|
+        ItemOrder.create(order_id: new_order.id, quantity: cart_item.quantity, item_id: cart_item.item_id)
+        cart_item.destroy
+      end
 
-    new_order.save
+  # Envoyer un email à l'user et à l'admin (user first)
+      OrderMailer.order_email(new_order, User.find(new_order.user_id)).deliver_now!
+      OrderMailer.admin_email(new_order, User.first).deliver_now!
 
-# Si le paiement n'a pas fonctionné
-rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to new_charge_path
+
+      # Si le paiement n'a pas fonctionné
+      rescue Stripe::CardError => e
+        flash[:error] = e.message
+        redirect_to new_charge_path
 
 end
 end
