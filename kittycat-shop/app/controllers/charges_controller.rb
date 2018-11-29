8767e787f4ce87661ad80c70d9ae3acc25685edf
TinQk <potie.quentin@gmail.com>
2018-11-29 16:23:38 +0100

    add before_action :authenticate_user!

--- kittycat-shop/app/controllers/charges_controller.rb
+++ kittycat-shop/app/controllers/charges_controller.rb
@@ -1,37 +1,40 @@
 class ChargesController < ApplicationController
+
+  before_action :authenticate_user!
+
   def new
-end
-
-def create
-  # STRIPE
-  @amount = (current_user.cart.total_price * 100).to_i
-
-  customer = Stripe::Customer.create(
-    :email => params[:stripeEmail],
-    :source  => params[:stripeToken]
-  )
-
-  charge = Stripe::Charge.create(
-    :customer    => customer.id,
-    :amount      => @amount,
-    :description => 'Rails Stripe customer',
-    :currency    => 'usd'
-  )
-
-# Créer un order et clear le panier
-  new_order = Order.new
-  new_order.user_id = current_user.id
-  new_order.save
-
-  current_user.cart.cart_items.each do |cart_item|
-    ItemOrder.create(order_id: new_order.id, quantity: cart_item.quantity, item_id: cart_item.item_id)
-    cart_item.destroy
   end
 
-# Si le paiement n'a pas fonctionné
-rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to new_charge_path
+  def create
+    # STRIPE
+    @amount = (current_user.cart.total_price * 100).to_i
 
-end
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'usd'
+    )
+
+  # Créer un order et clear le panier
+    new_order = Order.new
+    new_order.user_id = current_user.id
+    new_order.save
+
+    current_user.cart.cart_items.each do |cart_item|
+      ItemOrder.create(order_id: new_order.id, quantity: cart_item.quantity, item_id: cart_item.item_id)
+      cart_item.destroy
+    end
+
+    # Si le paiement n'a pas fonctionné
+    rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
+
 end
